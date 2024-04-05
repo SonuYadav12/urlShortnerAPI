cef76625cdbacb5701dd307691f5363ed195a429
Sonu Kumar Yadav <yadavsonukumar036@gmail.com>
2024-04-05 12:49:15 +0530

    testing

--- src/server.ts
+++ src/server.ts
@@ -10,12 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use("/api/",shortUrl);
 
